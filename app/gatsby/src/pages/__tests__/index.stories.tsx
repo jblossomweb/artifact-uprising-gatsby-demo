1406dff5c24cba6cd823227fd78d72b72ca63114
John Blossom <jblossom.web@gmail.com>
2020-07-17 16:43:19 -0600

    index page list products

--- app/gatsby/src/pages/__tests__/index.stories.tsx
+++ app/gatsby/src/pages/__tests__/index.stories.tsx
@@ -1,11 +1,30 @@
 import React from "react"
-import storyBuilder, { Stories } from "../../../utils/story-builder"
-import Index from "../index"
+import storyBuilder, {
+  KnobsInterface,
+  Stories,
+} from "../../../utils/story-builder"
+import mockQuery from "../../../__mocks__/query/index.query.json"
+import IndexPage, { Props, Data } from "../index"
 
-const story = () => () => <Index />
+const data: Data = mockQuery.data
+
+const sampleProps: Props = {
+  data,
+}
+
+const story = (knobProps: Props) => (
+  knobs: KnobsInterface,
+  props: Props = knobProps
+) => (
+  <IndexPage
+    data={knobs.object("data", props.data || {})}
+  />
+)
 
 export const stories: Stories = {
-  sample: story(),
+  sample: story({
+    ...sampleProps,
+  }),
 }
 
 storyBuilder(stories, "pages/index")
--- app/gatsby/src/pages/index.tsx
+++ app/gatsby/src/pages/index.tsx
@@ -1,23 +1,118 @@
 import React from "react"
-import { Link } from "gatsby"
+import { PageProps, Link, graphql } from "gatsby"
+import Img, { FluidObject } from "gatsby-image"
 
+import slugify from "../helpers/slugify"
 import Layout from "../layouts/default"
-import { GatsbyAstronaut } from "../components/images"
 import SEO from "../components/seo"
 
-const IndexPage: React.FC = () => (
+export type Data = {
+  db: {
+    products: {
+      id: number
+      title: string
+      imageUrl: string
+      price: number
+    }[]
+  }
+  page: {
+    pageNodes: {
+      context: {
+        imageId: string
+        productId: number
+      }
+    }[]
+  }
+  file: {
+    fileNodes: {
+      id: string
+      image: {
+        fluid: FluidObject
+      }
+    }[]
+  }
+}
+
+export interface Props extends Partial<PageProps<Data>> {
+  data: Data
+}
+
+const IndexPage: React.FC<Props> = ({
+  data: {
+    db: { products },
+    page: { pageNodes },
+    file: { fileNodes },
+  }
+}) => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <GatsbyAstronaut />
+    <div style={{ width: `100%`, display: `flex`, flexWrap: `wrap` }}>
+      {products.map(product => {
+        const path = `/product/${product.id}/${slugify(product.title)}`
+        const page = pageNodes.find(({ context }) => context.productId === product.id)
+        const { image } = fileNodes.find(({ id }) => id === page?.context.imageId)!
+        return (
+          <div key={product.id} style={{ width: `280px`, marginBottom: `1.45rem`, marginRight: `1.45rem` }}>
+            <div style={{ maxWidth: `280px`, maxHeight: `280px`, marginBottom: `1.45rem` }}>
+              <Link to={path}>
+                <Img fluid={image.fluid} />
+              </Link>
+            </div>
+            <div style={{ width: `100%`, display: `flex`, justifyContent: `space-between` }}>
+              <h6>
+                {product.title}
+              </h6>
+              <h6>
+                ${product.price.toFixed(2)}
+              </h6>
+            </div>
+            <div style={{ width: `100%` }}>
+              <button style={{ width: `100%`, cursor: `pointer`}}>
+                Add To Cart
+              </button>
+            </div>
+          </div>
+        )
+      })}
     </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/page-3/">Go to page 3</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
   </Layout>
 )
+
+export const query = graphql`
+  query {
+    db: postgres {
+      products: allProductsList {
+        id
+        title
+        imageUrl
+        price
+      }
+    }
+    page: allSitePage(filter: {context: {productId: { ne: null }}}) {
+      pageNodes: nodes {
+        context {
+          imageId
+          productId
+        }
+      }
+    }
+    file: allFile(filter: {dir: { ne: "/gatsby/src/images"}, extension: {in: ["png", "jpg", "gif"]}}) {
+      fileNodes: nodes {
+        id
+        image: childImageSharp {
+          fluid(maxWidth: 300, maxHeight: 300) {
+            aspectRatio
+            src
+            srcSet
+            sizes
+            base64
+            tracedSVG
+            srcSetWebp
+          }
+        }
+      }
+    }
+  }
+`
 
 export default IndexPage
